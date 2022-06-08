cba7419e05fa93309ff1062d63609160955cd450
ci7lus <7887955+ci7lus@users.noreply.github.com>
2022-06-09 02:42:34 +0900

    wine-staging: add

---
+++ Casks/wine-staging.rb
@@ -0,0 +1,56 @@
+cask "wine-staging" do
+  version "7.9"
+  sha256 "3e87456d77af84ecbf41a6521fde75e532173ae3ec745b27e5c52635c00c4415"
+
+  # Current winehq packages are deprecated and these are packages from
+  # the new maintainers that will eventually be pushed to Winehq.
+  # See https://www.winehq.org/pipermail/wine-devel/2021-July/191504.html
+  url "https://github.com/Gcenx/macOS_Wine_builds/releases/download/#{version}/wine-staging-#{version}-osx64.tar.xz",
+      verified: "https://github.com/Gcenx/macOS_Wine_builds/"
+  name "WineHQ-staging"
+  desc "Compatibility layer to run Windows applications"
+  homepage "https://wiki.winehq.org/MacOS"
+
+  livecheck do
+    url "https://github.com/Gcenx/macOS_Wine_builds/releases/"
+    strategy :page_match
+    regex(/wine[._-]stating[._-]v?(\d+(?:\.\d+)+)[._-]osx64\.tar\.xz/i)
+  end
+
+  conflicts_with cask: %w[
+    wine-stable
+    wine-devel
+    wine-crossover
+    wine-stable-legacy
+  ]
+
+  app "Wine Stating.app"
+  binary "#{appdir}/Wine Staging.app/Contents/Resources/start/bin/appdb"
+  binary "#{appdir}/Wine Staging.app/Contents/Resources/start/bin/winehelp"
+  binary "#{appdir}/Wine Staging.app/Contents/Resources/wine/bin/msiexec"
+  binary "#{appdir}/Wine Staging.app/Contents/Resources/wine/bin/notepad"
+  binary "#{appdir}/Wine Staging.app/Contents/Resources/wine/bin/regedit"
+  binary "#{appdir}/Wine Staging.app/Contents/Resources/wine/bin/regsvr32"
+  binary "#{appdir}/Wine Staging.app/Contents/Resources/wine/bin/wine"
+  binary "#{appdir}/Wine Staging.app/Contents/Resources/wine/bin/wine64"
+  binary "#{appdir}/Wine Staging.app/Contents/Resources/wine/bin/wineboot"
+  binary "#{appdir}/Wine Staging.app/Contents/Resources/wine/bin/winecfg"
+  binary "#{appdir}/Wine Staging.app/Contents/Resources/wine/bin/wineconsole"
+  binary "#{appdir}/Wine Staging.app/Contents/Resources/wine/bin/winedbg"
+  binary "#{appdir}/Wine Staging.app/Contents/Resources/wine/bin/winefile"
+  binary "#{appdir}/Wine Staging.app/Contents/Resources/wine/bin/winemine"
+  binary "#{appdir}/Wine Staging.app/Contents/Resources/wine/bin/winepath"
+  binary "#{appdir}/Wine Staging.app/Contents/Resources/wine/bin/wineserver"
+
+  caveats <<~EOS
+    #{token} supports both 32-bit and 64-bit. It is compatible with an existing
+    32-bit wine prefix, but it will now default to 64-bit when you create a new
+    wine prefix. The architecture can be selected using the WINEARCH environment
+    variable which can be set to either win32 or win64.
+
+    To create a new pure 32-bit prefix, you can run:
+      $ WINEARCH=win32 WINEPREFIX=~/.wine32 winecfg
+
+    See the Wine FAQ for details: https://wiki.winehq.org/FAQ#Wineprefixes
+  EOS
+end
